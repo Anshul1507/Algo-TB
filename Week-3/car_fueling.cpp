f85f8784772715153104c64dc143b4bf53daccee
Anshul1507 <ag1507anshul@gmail.com>
2019-12-05 02:45:24 +0530

    Week 3 >> Car Fueling

---
+++ Week-3/car_fueling.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#define ll long int
+#define HACKS std::ios::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define MAX 1e18+11
+#define PI 3.14159265358979323844
+#define SORT(v) sort(v.begin(),v.end())
+#define REVERSE(v) reverse(v.begin(),v.end()) 
+#define maxv(v) max_element(v.begin(),v.end())
+#define minv(v) min_element(v.begin(),v.end())
+#define GCD(m,n) __gcd(m,n)
+#define LCM(m,n) m*(n/GCD(m,n))
+#define inputarr(a,n) for(int i=0;i<n;i++) cin>>a[i]
+#define vec vector<ll>
+#define v_pair vector<pair<ll,ll>>
+#define display(x) for(auto& a : x) cout<<a<<" "; cout << endl;
+ 
+using namespace std;
+
+
+int solve(){
+  vec v;
+  ll d,m,n;
+  cin >> d >> m >> n;
+  
+  v.pb(0);
+  v.pb(d);
+  
+  ll NR=0,CR=0,a,flag=0;
+  for(int i=0;i<n;i++){
+     cin >> a;
+     v.pb(a);
+  }
+  SORT(v);
+  while(CR <= n){
+     int LR = CR;
+     while(CR<=n and (v[CR+1]-v[LR])<=m){
+        CR++;
+     }
+     if(CR == LR){
+        flag = 1;
+        break;
+     }
+     if(CR <= n){
+        NR++;
+     }
+  }
+  if(flag == 0){
+      cout << NR << endl;
+  }else{
+      cout << -1 << endl;
+  }
+  return 0;
+ 
+} 
+ 
+ 
+int main(){
+ HACKS
+ 
+ int t=1;
+// cin >> t;
+ 
+ while(t--){
+   solve();
+ }
+
+}
+ 
